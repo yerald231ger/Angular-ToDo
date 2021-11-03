bba97ccad8f579ec9d01d5318956831a0891fe6d
unknown <yerald231ger@gmail.com>
2021-11-03 11:04:45 -0600

    local storage

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { List } from 'src/app/models/list.model';
+import { AlertController } from '@ionic/angular';
 import { TodoService } from '../../services/todo.service';
 
 @Component({
@@ -12,11 +12,42 @@ export class Tab1Page {
 
 
   constructor(public todoService: TodoService,
-    private router: Router) {
+    private router: Router,
+    private alertCtrl: AlertController) {
   }
   
-  agregarList(){
-    this.router.navigateByUrl('/tabs/tab1/add');
+  async agregarList(){
+    // this.router.navigateByUrl('/tabs/tab1/add');
+
+    const alert = await this.alertCtrl.create({
+      header: 'Nueva lista',
+      inputs: [{
+        name: 'titulo',
+        type: 'text',
+        placeholder: 'Nombre de la lista'
+      }],
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancelar');
+        }
+      },{
+        text: 'Crear', 
+        handler: (data) => {
+          console.log(data);
+          if(data.titulo.length === 0){
+            return;
+          }
+
+          this.todoService.creatList(data.titulo);
+        }
+      }]
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
   }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,17 +3,32 @@ import { Injectable } from '@angular/core';
 import { List } from '../models/list.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  lists: List[] = [];
 
-  lists: List[] = [];  
+  constructor() {
+     this.getStorage();
+  }
 
-  constructor() { 
-    const lista1 = new List('Recolectar piedras del infinito');
-    const lista2 = new List('Heores a desaparecer');
-    this.lists.push(lista1, lista2);
+  creatList(title: string) {
+    const nuevaLista = new List(title);
 
-        
+    this.lists.push(nuevaLista);
+
+    this.saveStorage();
+  }
+
+  saveStorage() {
+    localStorage.setItem('data', JSON.stringify(this.lists));
+  }
+
+  getStorage()   {
+
+    if(localStorage.getItem('data')){
+      this.lists = JSON.parse(localStorage.getItem('data'));
+
+    }
   }
 }
