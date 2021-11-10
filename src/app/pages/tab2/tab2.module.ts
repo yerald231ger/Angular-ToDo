f4d46803d30b50f4d5dd353ea040194c046a4d6b
yerald231ger@gmail.com <yerald231ger@gmail.com>
2021-11-10 01:36:17 -0600

    declarar un modulo superior

--- src/app/pages/tab2/tab2.module.ts
+++ src/app/pages/tab2/tab2.module.ts
@@ -7,6 +7,7 @@ import { Tab2Page } from './tab2.page';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 import { ExploreContainerComponentModule } from 'src/app/explore-container/explore-container.module';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { ExploreContainerComponentModule } from 'src/app/explore-container/explo
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    ComponentsModule
   ],
   declarations: [Tab2Page]
 })
